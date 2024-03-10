44e9e246e1ff89a90ce2c6cd3c3da6e97118042c
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2024-03-10 08:15:32 -0500

    added react flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import "./App.css";
+import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./config/theme";
+import MainMindMap from "./components/MainMindMap";
 
 function App() {
   return (
-    <>
-      <p>Jide</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box component="main" style={styles.mainContianer}>
+        <MainMindMap />
+      </Box>
+    </ThemeProvider>
   );
 }
+
+/** @type {import("@mui/material").SxProps} */
+const styles = {
+  mainContianer: {
+    padding: 0,
+    margin: 0,
+    boxSizing: "border-box",
+  },
+  headAndMain: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+  },
+};
 
 export default App;
