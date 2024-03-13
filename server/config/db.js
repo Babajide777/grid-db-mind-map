a06fa438a5f4579b65e4f1da565c790f08224853
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2024-03-13 09:08:20 +0100

    changed container name

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const griddb = require("griddb-node-api");
 
-const containerName = "meal-planner";
+const containerName = "mind-map";
 
 const initStore = async () => {
   const factory = griddb.StoreFactory.getInstance();
