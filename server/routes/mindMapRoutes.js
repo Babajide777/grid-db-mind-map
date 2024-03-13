0f76035e0b5e4dfcb825ca7677972573f4535723
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2024-03-13 08:39:52 +0100

    edited all routes

--- server/routes/mindMapRoutes.js
+++ server/routes/mindMapRoutes.js
@@ -1,16 +1,16 @@
 const {
-  addMealPlan,
-  mealPlanDetails,
-  editMealPlan,
-  deleteMealPlan,
-  getAllMealPlans,
-} = require("../controllers/mealController");
+  addMealItem,
+  mapItemDetails,
+  editMapItem,
+  deleteMapItem,
+  getAllMapItems,
+} = require("../controllers/mindMapController");
 const router = require("express").Router();
 
-router.post("/add-map-item", addMealPlan);
-router.get("/meal-detail/:id", mealPlanDetails);
-router.put("/edit-meal/:id", editMealPlan);
-router.delete("/delete-meal/:id", deleteMealPlan);
-router.get("/all-meals", getAllMealPlans);
+router.post("/add-map-item", addMealItem);
+router.get("/map-detail/:id", mapItemDetails);
+router.put("/edit-map-item/:id", editMapItem);
+router.delete("/delete-map-item/:id", deleteMapItem);
+router.get("/all-map-items", getAllMapItems);
 
 module.exports = router;
