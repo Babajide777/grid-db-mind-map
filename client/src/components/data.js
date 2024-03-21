e8f2752599e7b9d851af75515d92e362617afb98
kennyoyaf <kehindeoyafemi@gmail.com>
2024-03-21 14:38:06 -0700

    react flow completed

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -6,9 +6,9 @@ const Details = [
       required: "source is required.",
       pattern: {
         value: /^.*$/,
-        message: "Source must have an alphanumeric value.",
-      },
-    },
+        message: "Source must have an alphanumeric value."
+      }
+    }
   },
   {
     name: "target",
@@ -17,34 +17,34 @@ const Details = [
       required: "target is required.",
       pattern: {
         value: /^.*$/,
-        message: "Target must have an alphanumeric value.",
-      },
-    },
+        message: "Target must have an alphanumeric value."
+      }
+    }
   },
   {
     name: "positionX",
     errorMessage: "Please enter a valid x.",
     errorParams: {
       required: "x is required.",
-      pattern: { value: /^\d+$/, message: "x must be numeric." },
-    },
+      pattern: { value: /^\d+$/, message: "x must be numeric." }
+    }
   },
   {
     name: "positionY",
     errorMessage: "Please enter a valid y.",
     errorParams: {
       required: "y is required.",
-      pattern: { value: /^\d+$/, message: "y must be numeric." },
-    },
+      pattern: { value: /^\d+$/, message: "y must be numeric." }
+    }
   },
   {
     name: "label",
     errorMessage: "Please enter a valid label.",
     errorParams: {
       required: "label is required.",
-      pattern: { value: /^[a-zA-Z]+$/g, message: "label must be alphabets." },
-    },
-  },
-];
+      pattern: { value: /^[a-zA-Z]+$/g, message: "label must be alphabets." }
+    }
+  }
+]
 
-export { Details };
+export { Details }
