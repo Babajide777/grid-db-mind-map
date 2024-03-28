010bca155439987a7ab24d214772546b06d25a35
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2024-03-28 02:06:12 +0100

    added the base url for server

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors("*"));
+
+app.get("/", (req, res) => {
+  res.send("GridDB mind map Backend API");
+});
+
 app.use("/api", require("./routes/mindMapRoutes"));
 
 app.listen(PORT, () => {
